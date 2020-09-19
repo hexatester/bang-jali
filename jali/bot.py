b35803f1b0f984f0ac0de77606dcc94452554faa
hexatester <revolusi147id@gmail.com>
2020-09-19 19:10:55 +0700

    improve user_joined

--- jali/bot.py
+++ jali/bot.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient
 from telethon.tl.custom import Message
 from telethon.tl.types import Chat, User
 from telethon.events import NewMessage, ChatAction
-from typing import Dict, List, Union
+from typing import Dict, Union
 
 from jali.config import ID, API_ID, API_HASH, ADMIN
 from jali.utils import get_tags, unindent
@@ -13,7 +13,8 @@ TAGS: Dict[str, str] = get_tags()
 
 jali = TelegramClient(ID, API_ID, API_HASH)
 
-NEW_USERS: List[User] = []
+# chat_id -> user_id -> name
+NEW_USERS: Dict[int, Dict[int, str]] = {}
 WELCOME_PER = 10
 
 
@@ -26,7 +27,8 @@ last_hastag = int(time.time()) - 60
 
 
 @jali.on(
-    NewMessage(forwards=False, pattern=r'^!#[a-z]+$',
+    NewMessage(forwards=False,
+               pattern=r'^!#[a-z]+$',
                func=lambda e: e.is_reply))
 async def hastag(event: Union[NewMessage.Event, Message]):
     global last_hastag
@@ -41,17 +43,19 @@ async def hastag(event: Union[NewMessage.Event, Message]):
 @jali.on(ChatAction)
 async def chat_action(event: Union[ChatAction.Event, Message]):
     if event.user_joined:
+        chat: Chat = await event.get_chat()
         user: User = await event.get_user()
-        if user not in NEW_USERS:
-            NEW_USERS.append(user)
-        if len(NEW_USERS) >= WELCOME_PER:
+        NS = NEW_USERS.get(chat.id, {})
+        if user.id not in NS:
+            name = user.first_name
+            if user.last_name:
+                name += ' ' + user.last_name
+            NS[user.id] = name
+        if len(NS) >= WELCOME_PER:
             chat: Chat = await event.get_chat()
             text = 'Selamat datang di group'
-            for user in NEW_USERS:
-                name = user.first_name
-                if user.last_name:
-                    name += ' ' + user.last_name
-                text += f", [{name}](tg://user?id={user.id})"
+            for id_, name in NS.items():
+                text += f", [{name}](tg://user?id={id_})"
             text += '''. 😃\n
             **Feel free to sharing seputar UT, No SARA, No SPAM, No Iklan.**
             [Kumpulan Akses Bahan Ajar](https://t.me/UniversitasTerbuka/37415)
@@ -63,4 +67,5 @@ async def chat_action(event: Union[ChatAction.Event, Message]):
                 unindent(text),
                 link_preview=False,
             )
-            NEW_USERS.clear()
+            NS.clear()
+        NEW_USERS[chat.id] = NS
