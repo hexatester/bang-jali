f4ab1c7d74b5a048b71d1f4f45fac539b4884e16
hexatester <revolusi147id@gmail.com>
2020-09-19 19:10:22 +0700

    improve config

--- jali/config.py
+++ jali/config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 JALI_PATH = os.path.dirname(os.path.abspath(__file__))
 BASE_PATH = os.path.abspath(os.path.join(JALI_PATH, os.pardir))
+ENV_PATH = os.path.join(BASE_PATH, '.env')
+load_dotenv(ENV_PATH)
+
 ID = os.environ.get('id')
 API_ID = os.environ.get('api_id')
 API_HASH = os.environ.get('api_hash')
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from jali import jali
-from dotenv import load_dotenv
-load_dotenv()
 
 jali.start()
 jali.run_until_disconnected()
